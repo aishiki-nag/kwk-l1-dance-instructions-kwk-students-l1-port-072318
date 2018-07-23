9c2a80d5316762b36c82f84a431d3359c708d8d6
aishiki-nag <aishiki.nag@icloud.com>
2018-07-23 23:21:03 +0000

    automatically backed up by learn

--- dance.rb
+++ dance.rb
@@ -1,2 +1,9 @@
-def 
-  puts " Plant legsfar apart"
+def lower body
+  puts " Plant legs far apart, bend knees slighlty and keep posture loose"
+end 
+
+def base_footwork 
+  puts " Lift right foot"
+  puts " Return right foot to the ground"
+  puts " Finishing with a small skip-step backwards"
+end
