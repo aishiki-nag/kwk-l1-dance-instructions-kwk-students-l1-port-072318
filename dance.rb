f2d3e402d73149bc7fee9bd2ab702ecdc01cd818
aishiki-nag <aishiki.nag@icloud.com>
2018-07-23 23:36:40 +0000

    automatically backed up by learn

--- dance.rb
+++ dance.rb
@@ -1,9 +1,34 @@
 def lower body
   puts " Plant legs far apart, bend knees slighlty and keep posture loose"
 end 
-
+lower body
 def base_footwork 
   puts " Lift right foot"
   puts " Return right foot to the ground"
   puts " Finishing with a small skip-step backwards"
+end 
+base_footwork
+def skip-step left
+  puts "Lower left foot to the groung"
+  puts "Bounce left foot back up slightly, kicking it a few inches"
+  puts "Left, right,left, right"
 end
+skip-step left
+def skip-step right
+  puts "Lower right foot to ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches"
+  puts "Right, left, right, left"
+end
+skip-step right
+def bob the reins 
+  puts " Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold tem toghether"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated 8 times. "
+end
+bob the reins
+def lasso 
+  puts "Start arm movement by holding left arm so the back of your wrist is neear your chin, pointing your left ebiw straight left"
+  puts "Lift right ar up and point right elbow diagonally to te right"
+  puts " Raise right forearm so that it points up and whip it in small circes to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated 8 times"
+end
+lasso
